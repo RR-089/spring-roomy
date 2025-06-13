ca64fa0aac42dc3aa91822c959b9c41c5da25933
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-06-13 20:12:18 +0700

    add: cors config

---
+++ src/main/java/com/example/roomy/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.example.roomy.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Value("${cors.allowedOriginPattern}")
+    private List<String> allowedOriginPattern;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+        config.setAllowedOriginPatterns(allowedOriginPattern);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
