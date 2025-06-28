7b64450a5684ce2462666319b00f706624ef045b
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-06-28 17:17:02 +0700

    add: custom annotations

---
+++ src/main/java/com/example/roomy/annotation/RoomMasterOnly.java
@@ -0,0 +1,14 @@
+package com.example.roomy.annotation;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@PreAuthorize("hasRole('ROOM_MASTER')")
+public @interface RoomMasterOnly {
+}
--- src/main/java/com/example/roomy/controller/ProtectedController.java
+++ src/main/java/com/example/roomy/controller/ProtectedController.java
@@ -1,10 +1,10 @@
 package com.example.roomy.controller;
 
+import com.example.roomy.annotation.AdminOnly;
 import com.example.roomy.dto.common.ResponseDTO;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProtectedController {
 
     @GetMapping
-    @PreAuthorize("hasRole('USER')")
+    //@PreAuthorize("hasRole('USER')")
     //@PreAuthorize("hasAuthority('ROLE_USER')")
+    //@UserOnly
+    //@RoomMasterOnly
+    @AdminOnly
     public ResponseEntity<?> helloWorldProtected() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
